f21043de96ca2a068e8313b911b4ff3938b12298
Mohit Kumar <mk4227525@gmail.com>
2022-06-12 00:50:29 +0530

    wait for db command config

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,10 +2,12 @@
 Calculator functions
 """
 
-def add(x,y):
-    """Add x and y and return result."""
-    return x + y 
 
-def subs(x,y):
+def add(x, y,):
+    """Add x and y and return result."""
+    return x + y
+
+
+def subs(x, y,):
     """ Subtacts two numbers"""
     return x - y
--- app/app/tests.py
+++ app/app/tests.py
@@ -1,23 +1,24 @@
 """
-Sample Test 
+Sample Test
 """
 
 from django.test import SimpleTestCase
 
 from app import calc
 
+
 class Calctest(SimpleTestCase):
     """Test the calc module"""
 
     def test_add_numbers(self):
         """Test adding numbers together."""
-        res = calc.add(5,6)
+        res = calc.add(5, 6)
 
-        self.assertEqual(res,11)
-    
+        self.assertEqual(res, 11)
+
     def test_subs_numbers(self):
         """Test subs numbers."""
 
-        res = calc.subs(6,5)
+        res = calc.subs(6, 5)
 
-        self.assertEqual(res,1)
+        self.assertEqual(res, 1)
