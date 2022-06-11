7d0241adba4af893424b938e8db454256dfa0ac4
Mohit Kumar <mk4227525@gmail.com>
2022-06-11 23:32:11 +0530

    adde postgres in docker

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculator functions
+"""
+
+def add(x,y):
+    """Add x and y and return result."""
+    return x + y 
+
+def subs(x,y):
+    """ Subtacts two numbers"""
+    return x - y
---
+++ app/app/tests.py
@@ -0,0 +1,23 @@
+"""
+Sample Test 
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+class Calctest(SimpleTestCase):
+    """Test the calc module"""
+
+    def test_add_numbers(self):
+        """Test adding numbers together."""
+        res = calc.add(5,6)
+
+        self.assertEqual(res,11)
+    
+    def test_subs_numbers(self):
+        """Test subs numbers."""
+
+        res = calc.subs(6,5)
+
+        self.assertEqual(res,1)
