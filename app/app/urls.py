965a197193bde9fe3b7810880523caf3c2a8cc60
Mohit Kumar <mk4227525@gmail.com>
2022-06-14 00:11:58 +0530

    manage user related code

--- app/app/urls.py
+++ app/app/urls.py
@@ -26,6 +26,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='api-schema'), name='api-docs'),
-    path('api/user', include('user.urls'))
+    path('api/user/', include('user.urls'))
 
 ]
